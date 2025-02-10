7072a9bcb8cbe69b94fbaa07ebecc42557c6cd4e
llfWilliam <1047668716@qq.com>
2025-02-10 16:51:04 +0800

    实现聊天室

--- build/Desktop_Qt_6_5_3_MinGW_64_bit-Debug/CMakeFiles/clean_additional.cmake
+++ build/Desktop_Qt_6_5_3_MinGW_64_bit-Debug/CMakeFiles/clean_additional.cmake
@@ -3,8 +3,11 @@ cmake_minimum_required(VERSION 3.16)
 
 if("${CONFIG}" STREQUAL "" OR "${CONFIG}" STREQUAL "Debug")
   file(REMOVE_RECURSE
+  "CMakeFiles\\ChatServer_autogen.dir\\AutogenUsed.txt"
+  "CMakeFiles\\ChatServer_autogen.dir\\ParseCache.txt"
   "CMakeFiles\\Chatroom_autogen.dir\\AutogenUsed.txt"
   "CMakeFiles\\Chatroom_autogen.dir\\ParseCache.txt"
+  "ChatServer_autogen"
   "Chatroom_autogen"
   )
 endif()
