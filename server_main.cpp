f2afff8e9fcd9a51f1ab74c73b31518dc974a5f3
llfWilliam <1047668716@qq.com>
2025-02-10 17:05:38 +0800

    彻底解决了构建问题，待测试

---
+++ server_main.cpp
@@ -0,0 +1,11 @@
+#include <QCoreApplication>
+#include "server.h"
+
+int main(int argc, char *argv[]) {
+    QCoreApplication a(argc, argv);
+    int port = 12345;
+    ChatServer server;
+    server.startServer(port);  // 确保 Server 类有 startServer() 方法
+
+    return a.exec();
+}
